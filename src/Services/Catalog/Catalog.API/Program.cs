6ed3db6257068099f8120ec5f1ffa38159a19e4a
jadaunharsh <jadaunharsh92@gmail.com>
2024-10-02 16:44:26 +0530

    Healthchecks, Docker containerization and Basket API setup added.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,7 @@
 
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
@@ -23,11 +26,20 @@ if(builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomerExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 //configure the HTTP request pipeline
 app.MapCarter();
 
 app.UseExceptionHandler(option => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
